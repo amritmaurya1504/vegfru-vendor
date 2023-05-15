73eff448fced1e59632c8f40f2f9c6cafd8d5d92
Amrit Raj <amrit.raj1504@gmail.com>
2023-05-15 22:27:33 +0530

    product list changes

--- src/components/store/ProductList.jsx
+++ src/components/store/ProductList.jsx
@@ -12,119 +12,92 @@ import {
   Badge,
 } from "@chakra-ui/react";
 import { Tooltip } from "react-tooltip";
+import { people } from "../dummy";
 
 const ProductList = () => {
   const [status, setStatus] = useState(true);
 
   return (
     <>
-      <div className="px-5 md:px-24 py-8 rounded-lg">
-        <div className="flex justify-between items-center mt-12">
-          <div>
-            <h3 className="text-4xl font-semibold mb-2">
-              Product <span className="text-green-500"> List</span>
-            </h3>
-            <div class="border-b-2 border-black mt-4 w-8"></div>
+      <div className="px-4 sm:px-6 lg:px-24">
+        <div className="sm:flex sm:items-center">
+          <div className="sm:flex-auto">
+            <h1 className="text-xl font-semibold text-gray-900" >Users</h1>
+            <p className="mt-2 text-sm text-gray-700">
+              A list of all the users in your account including their name, title, email and role.
+            </p>
+          </div>
+          <div className="mt-4 sm:mt-0 sm:ml-16 sm:flex-none">
+            <button
+              type="button"
+              className="inline-flex items-center justify-center rounded-md border border-transparent bg-[#33c94a] px-4 py-2 text-sm font-medium text-white shadow-sm hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 sm:w-auto"
+            >
+              Add Product
+            </button>
           </div>
         </div>
-        <div className="mt-10 bg-white">
-          <TableContainer className="">
-            <Table variant="simple">
-              <TableCaption placement="top">
-                List of all the products available in store
-              </TableCaption>
-              <Thead>
-                <Tr>
-                  <Th>S No.</Th>
-                  <Th>Product Name</Th>
-                  <Th isNumeric>Price</Th>
-                  <Th>Unit</Th>
-                  <Th>Status</Th>
-                </Tr>
-              </Thead>
-              <Tbody>
-                <Tr>
-                  <Td isNumeric style={{ textAlign: "left" }}>
-                    1
-                  </Td>
-                  <Td>Potato</Td>
-                  <Td isNumeric>25</Td>
-                  <Td> kg</Td>
-                  <Td>
-                    {" "}
-                    {status ? (
-                      <Badge
-                        variant="outline"
-                        colorScheme="green"
-                        style={{ cursor: "pointer" }}
-                        onClick={() => setStatus(!status)}
-                        data-tooltip-id="change status"
-                        data-tooltip-content="Click to change status"
-                        data-tooltip-place="top"
-                      >
-                        Available
-                        <Tooltip id="change status" />
-                      </Badge>
-                    ) : (
-                      <Badge
-                        variant="outline"
-                        colorScheme="red"
-                        style={{ cursor: "pointer" }}
-                        onClick={() => setStatus(!status)}
-                        data-tooltip-id="change status"
-                        data-tooltip-content="Click to change status"
-                        data-tooltip-place="top"
-                      >
-                        Out of Stock
-                        <Tooltip id="change status" />
-                      </Badge>
+        <div className="mt-8 flex flex-col">
+          <div className="-my-2 -mx-4 overflow-x-auto sm:-mx-6 lg:-mx-8">
+            <div className="inline-block min-w-full py-2 align-middle md:px-6 lg:px-8">
+              <div className="overflow-hidden shadow ring-1 ring-black ring-opacity-5 md:rounded-lg">
+                <table className="min-w-full divide-y divide-gray-300">
+                  <thead className="bg-green-100">
+                    <tr>
+                      <th scope="col" className="py-3.5 pl-4 pr-3 text-left text-sm font-semibold text-gray-900 sm:pl-6">
+                        SNo
+                      </th>
+                      <th scope="col" className="px-3 py-3.5 text-left text-sm font-semibold text-gray-900">
+                        Product Name
+                      </th>
+                      <th scope="col" className="px-3 py-3.5 text-left text-sm font-semibold text-gray-900">
+                        Unit
+                      </th>
+                      <th scope="col" className="px-3 py-3.5 text-left text-sm font-semibold text-gray-900">
+                        Status
+                      </th>
+                      <th scope="col" className="px-3 py-3.5 text-left text-sm font-semibold text-gray-900">
+                        Party Logo
+                      </th>
+                    </tr>
+                  </thead>
+                  <tbody className="divide-y divide-gray-200 bg-white">
+                    {people?.map((item, i) => {
+                      return (
+                        <>
+                          <tr key={i}>
+                            <td className="whitespace-nowrap py-4 pl-4 pr-3 text-sm sm:pl-6">
+                              <div className="flex items-center">
+                                <div className="h-10 w-10 flex-shrink-0">
+                                  <img className="h-10 w-10 rounded-full" src={`${item.image}`} alt="" />
+                                </div>
+                                <div className="ml-4">
+                                  <div className="font-medium text-gray-900">{item.name}</div>
+                                  {/* <div className="text-gray-500">{person.email}</div> */}
+                                </div>
+                              </div>
+                            </td>
+                            <td className="whitespace-nowrap px-3 py-4 text-sm text-gray-500">
+                              <div className="text-gray-900">{item.title}</div>
+
+                            </td>
+                            <td className="whitespace-nowrap px-3 py-4 text-sm text-gray-500">
+                              <div className="text-gray-500">{item.role}</div>
+                            </td>
+                            <td className="whitespace-nowrap px-3 py-4 text-sm text-gray-500">
+                              <span className="inline-flex rounded-full bg-green-100 px-2 text-xs font-semibold leading-5 text-green-800">
+                                Active
+                              </span>
+                            </td>
+                          </tr>
+                        </>
+                      )
+                    }
                     )}
-                  </Td>
-                </Tr>
-                <Tr>
-                  <Td isNumeric style={{ textAlign: "left" }}>
-                    2
-                  </Td>
-                  <Td>Tomato</Td>
-                  <Td isNumeric>15</Td>
-                  <Td> kg</Td>
-                  <Td>
-                    {" "}
-                    <Badge variant="outline" colorScheme="red">
-                      Out of Stock
-                    </Badge>
-                  </Td>
-                </Tr>
-                <Tr>
-                  <Td isNumeric style={{ textAlign: "left" }}>
-                    3
-                  </Td>
-                  <Td>Potato</Td>
-                  <Td isNumeric>25</Td>
-                  <Td> kg</Td>
-                  <Td>
-                    {" "}
-                    <Badge variant="outline" colorScheme="red">
-                      Out of Stock
-                    </Badge>
-                  </Td>
-                </Tr>
-                <Tr>
-                  <Td isNumeric style={{ textAlign: "left" }}>
-                    4
-                  </Td>
-                  <Td>Potato</Td>
-                  <Td isNumeric>25</Td>
-                  <Td> kg</Td>
-                  <Td>
-                    <Badge variant="outline" colorScheme="green">
-                      Available
-                    </Badge>
-                  </Td>
-                </Tr>
-              </Tbody>
-            </Table>
-          </TableContainer>
+                  </tbody>
+                </table>
+              </div>
+            </div>
+          </div>
         </div>
       </div>
     </>
